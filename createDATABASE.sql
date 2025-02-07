3b3fca378b38aeb4012c972f7a8ef5a349b82dd3
Mohammed FEDAILY <mohammed.fedaily@Digital-Grenoble.local>
2025-02-07 14:42:08 +0100

    feat: add SQL tables creation script

--- createDATABASE.sql
+++ createDATABASE.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `SquareGames_db`;
 
 -- Level 0 Tables 
+
 CREATE TABLE `SquareGames_db`.`Currency` (
     `id_currency` INT PRIMARY KEY AUTO_INCREMENT,
     `symbol` VARCHAR(10) NOT NULL,
@@ -64,4 +65,63 @@ CREATE TABLE `SquareGames_db`.`TokenPack` (
     `packName` VARCHAR(255) NOT NULL,
     `packPrice` DECIMAL(10,2) NOT NULL,
     `userToken` INT DEFAULT 0
+);
+
+-- Level 2 Tables
+
+CREATE TABLE `SquareGames_db`.`GamePlay` (
+    `id_game_play` INT PRIMARY KEY AUTO_INCREMENT,
+    `game_start` DATETIME NOT NULL,
+    `game_end` DATETIME,
+    `id_game` INT,
+    FOREIGN KEY (`id_game`) REFERENCES `Game`(`id_game`)
+);
+
+CREATE TABLE `SquareGames_db`.`Pawn` (
+    `id_pawn` INT PRIMARY KEY AUTO_INCREMENT,
+    `id_pawnType` INT,
+    `id_board` INT,
+    FOREIGN KEY (`id_pawnType`) REFERENCES `PawnType`(`id_pawnType`),
+    FOREIGN KEY (`id_board`) REFERENCES `Board`(`id_board`)
+);
+
+
+-- Level 3 Tables
+
+CREATE TABLE `SquareGames_db`.`UserGames` (
+    `id_userInfo` INT,
+    `id_game_play` INT,
+    PRIMARY KEY (`id_userInfo`, `id_game_play`),
+    FOREIGN KEY (`id_userInfo`) REFERENCES `UserInfo`(`id_userInfo`),
+    FOREIGN KEY (`id_game_play`) REFERENCES `GamePlay`(`id_game_play`)
+);
+
+CREATE TABLE `SquareGames_db`.`PawnMove` (
+    `id_pawn` INT,
+    `id_game_play` INT,
+    `x` INT NOT NULL,
+    `y` INT NOT NULL,
+    `move_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id_pawn`, `id_game_play`, `move_date`),
+    FOREIGN KEY (`id_pawn`) REFERENCES `Pawn`(`id_pawn`),
+    FOREIGN KEY (`id_game_play`) REFERENCES `GamePlay`(`id_game_play`)
+);
+
+CREATE TABLE `SquareGames_db`.`Buy` (
+    `id_userInfo` INT,
+    `id_tokenPack` INT,
+    `purchase_date` DATE NOT NULL,
+    `quantity` INT NOT NULL,
+    PRIMARY KEY (`id_userInfo`, `id_tokenPack`, `purchase_date`),
+    FOREIGN KEY (`id_userInfo`) REFERENCES `UserInfo`(`id_userInfo`),
+    FOREIGN KEY (`id_tokenPack`) REFERENCES `TokenPack`(`id_tokenPack`)
+);
+
+CREATE TABLE `SquareGames_db`.`Cost` (
+    `id_tokenPack` INT,
+    `id_currency` INT,
+    `price` DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (`id_tokenPack`, `id_currency`),
+    FOREIGN KEY (`id_tokenPack`) REFERENCES `TokenPack`(`id_tokenPack`),
+    FOREIGN KEY (`id_currency`) REFERENCES `Currency`(`id_currency`)
 );
