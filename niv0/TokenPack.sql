a2fa63099f69de45bf7713bec4f2f7b03bfbf487
apasquier00 <alexandre.pasquier@le-campus-numerique.fr>
2025-02-07 13:26:39 +0100

    Game et TockenPack

---
+++ niv0/TokenPack.sql
@@ -0,0 +1,9 @@
+CREATE TABLE TockenPack
+(
+    id_tockenPack int PRIMARY KEY NOT NULL,
+    endingDate DATE,
+    startingDate DATE,
+    tokenAmount INT,
+    packName VARCHAR(255),
+    packPrice INT
+) ;
