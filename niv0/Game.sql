a3bd5740fe2713ab6cc4c848c5743daaf11e2dbd
Mohammed FEDAILY <mohammed.fedaily@Digital-Grenoble.local>
2025-02-07 14:39:20 +0100

    Delete unneeded filess

--- niv0/Game.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `SquareGames_db`.`Game`
-(
-    id_game int PRIMARY KEY NOT NULL,
-    isFavoritePawn BOOLEAN,
-    isGameLocal BOOLEAN,
-    gameRequiredAge INT,
-    isParentallyRestricted BOOLEAN,
-    gameDifficulty INT
-) ;
--- niv0/TokenPack.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE TockenPack
-(
-    id_tockenPack int PRIMARY KEY NOT NULL,
-    endingDate DATE,
-    startingDate DATE,
-    tokenAmount INT,
-    packName VARCHAR(255),
-    packPrice INT
-) ;
